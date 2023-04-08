3c3c60f26d534c66414dea811e0355b6e1a38f7b
ShlenkinVV <mr.shlenkin@mail.ru>
2023-04-08 14:15:09 +0400

    feat : check kab unique

--- djangoInvent/invent/models.py
+++ djangoInvent/invent/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Kab(models.Model):
-    num = models.CharField(max_length=10)
+    num = models.CharField(max_length=10, unique=True, verbose_name="номер")
 
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Kab(models.Model):
         
 class Inventar(models.Model):
     name = models.CharField(max_length=40)
-    num = models.CharField(max_length=40)
+    num = models.CharField(max_length=40, verbose_name="номер")
     num_kab = models.ForeignKey(Kab, on_delete=models.DO_NOTHING)
     response = models.CharField(max_length=40)
     count = models.CharField(max_length=5)
