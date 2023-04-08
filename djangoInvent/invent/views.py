b4a1269e2be01cfcca2dbbf0e147834c4fdd0883
ShlenkinVV <mr.shlenkin@mail.ru>
2023-04-08 13:32:55 +0400

    feat : add search

--- djangoInvent/invent/views.py
+++ djangoInvent/invent/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from .models import Inventar
+from django.db.models import Q
 
 def MyView(request):
-    
-    query_results = Inventar.objects.all()
+    search_post = request.GET.get('search')
+
+    if search_post:
+        posts = Inventar.objects.filter(Q(name__icontains=search_post))
+        
+    else:
+        posts = Inventar.objects.all()
+
+    #fields = [f for f in table._meta.fields]
+    #queries = [Q(**{f.name: SEARCH_TERM}) for f in fields]
+
+
+   # qs= Q()
+    #for query in queries:
+   #     qs = qs | query
+
+
+   # query_results = Inventar.objects(posts)
     # for q in query_results :
     #     print(q.name, q.num, q.num_kab, )
 
-    return render(request, "list.html", {"query_results":query_results})
+    return render(request, "list.html", {"query_results":posts})
 
 
 # Create your views here.
