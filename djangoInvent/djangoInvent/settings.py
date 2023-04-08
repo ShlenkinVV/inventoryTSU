0ce67a1aedce0e244fbb16da25c8122675f6f0d3
ShlenkinVV <mr.shlenkin@mail.ru>
2023-04-08 13:31:43 +0400

    feat : add search

--- djangoInvent/djangoInvent/settings.py
+++ djangoInvent/djangoInvent/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'djangoInvent.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'Inventarizate',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'F54036',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+}
 }
 
 
